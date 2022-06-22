4aff0d73e46cc2a72c201408a5e6d85e3109e1a8
Danika1808 <65758418+Danika1808@users.noreply.github.com>
2022-06-23 02:38:11 +0300

    буба

---
+++ App1/GraphQlClient/ResponseType.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphQlClient
+{
+    public class ResponseType
+    {
+        public Value Products { get; set; }
+    }
+
+    public class Value
+    {
+        public int TotalCount { get; set; }
+        public ICollection<Product> Items { get; set; }
+    }
+}
